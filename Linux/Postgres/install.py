94c0b6dcda5d9e2eaf7d4993201b437aceb057a3
Marcus Willock <crazcalm@gmail.com>
2014-06-21 15:47:41 -0400

    creating a username for postgres

---
+++ Linux/Postgres/install.py
@@ -0,0 +1,23 @@
+from subprocess import call
+import os
+
+def install():
+    """
+    installs postgresql and postgressql-contrib
+    """
+    command = 'sudo apt-get install postgresql postgresql-contrib'
+    command_list = command.split(" ")
+    call(command.split(" "))
+
+def set_db_username():
+    """
+    Uses your login in name from your computer to create a Postgres username
+    """
+    username = os.getlogin()
+    command = "sudo -u postgres createuser --superuser %s" % username
+    command_list = command.split(" ")
+    call(command_list)
+
+if __name__ == "__main__":
+    install()
+    set_db_username()
